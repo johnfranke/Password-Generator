3dde7e7a1b677dd719dddf4914f7c4d38babfe3d
John Franke <johnrfrankejr@gmail.com>
2019-12-18 18:31:13 -0600

    updated length if statement and else alert and refresh page

--- script.js
+++ script.js
@@ -61,45 +61,58 @@ var upperCharArray = [
 ];
 
 var length = prompt("How many characters do you want your password to be?");
-console.log(length);
-var specialChar = confirm("Do you want special characters in your password?");
-console.log(specialChar);
-var numberChar = confirm("Do you want numbers in your password?");
-console.log(numberChar);
-var lowerChar = confirm("Do you want lowercase letters in your password?");
-console.log(lowerChar);
-var upperChar = confirm("Do you want uppercase letters in your password?");
-console.log(upperChar);
-
-var password = [""];
-for (var i = 0; i < length; i++) {
-	if (lowerChar === true) {
-	var randomLowerChar =
-		lowerCharArray[Math.floor(Math.random() * lowerCharArray.length)];
+	if (length > 7 && length < 129) {
+	console.log(length);
+	var specialChar = confirm("Do you want special characters in your password?");
+	console.log(specialChar);
+	var numberChar = confirm("Do you want numbers in your password?");
+	console.log(numberChar);
+	var lowerChar = confirm("Do you want lowercase letters in your password?");
+	console.log(lowerChar);
+	var upperChar = confirm("Do you want uppercase letters in your password?");
+	console.log(upperChar);
+	
+	var password = [""];
+	for (var i = 0; i < length; i++) {
+		if (lowerChar === true) {
+		var randomLowerChar =
+			lowerCharArray[Math.floor(Math.random() * lowerCharArray.length)];
+		}
+		if (numberChar === true) {
+		var randomNumber = Math.floor(Math.random() * numberArray.length);
+		}
+		if (upperChar === true) {
+		var randomUpperChar =
+			upperCharArray[Math.floor(Math.random() * upperCharArray.length)];
+		}
+		if (specialChar === true) { 
+		var randomSpecialChar =
+			specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
+		}
+		password.push(randomUpperChar);
+	
+		password.push(randomLowerChar);
+	
+		password.push(randomNumber);
+	
+		password.push(randomSpecialChar);
 	}
-	if (numberChar === true) {
-	var randomNumber = Math.floor(Math.random() * numberArray.length);
+	
+	password = password.join("").slice(0,length)
+	console.log("Password is " + password);
+	console.log("password length is " + password.length);
+	
+	alert("Password is: " + password);
+
+
+
+
+	} else {
+		alert('You must enter a length between 8 and 128');
+		location.reload();
 	}
-	if (upperChar === true) {
-	var randomUpperChar =
-		upperCharArray[Math.floor(Math.random() * upperCharArray.length)];
-	}
-	if (specialChar === true) { 
-	var randomSpecialChar =
-		specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
-	}
-	password.push(randomUpperChar);
 
-	password.push(randomLowerChar);
 
-	password.push(randomNumber);
 
-	password.push(randomSpecialChar);
-}
 
-password = password.join("").slice(0,length)
-console.log("Password is " + password);
-console.log("password length is " + password.length);
-
-alert("Password is: " + password);
 
